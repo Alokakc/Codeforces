85aeee789c29f01970f1316117fa91486a3f92c7
ALOK <alokakc012@gmail.com>
2024-09-03 01:54:12 +0530

    change scanner variable name

--- A_Park_Lighting.java
+++ A_Park_Lighting.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 public class A_Park_Lighting {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int t = input.nextInt();
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
         while(t-->0) {
-            int n = input.nextInt();
-            int m = input.nextInt();
+            int n = in.nextInt();
+            int m = in.nextInt();
             int numberOflights = (n/2)*m;
             if(n%2!=0) {
                 if(m%2==0) {
